c116664b92834464829246de518b9b6bc814d750
Nathan Chiverton <nathan@dropless.co.uk>
2022-09-28 15:52:10 +0100

    only show technologies on job descriptions

--- src/components/about/experience-data.tsx
+++ src/components/about/experience-data.tsx
@@ -12,11 +12,7 @@ export const experienceData: JobExperienceData[] = [
     beginDate: moment("2021-05").format("MM/YYYY"),
     jobTitle: "Lead Portal Developer",
     duties: [
-      `
-  Duties include: Maintenance (Technology
-  upgrades/improvements), product
-  roadmapping & code reviews`,
-      `Technologies include: React, React Native,
+      `React, React Native,
   Angular | RxJs, MobX | SASS, JSS | Node, Koa,
   Sequelize/SQL.`,
     ],
@@ -27,11 +23,7 @@ export const experienceData: JobExperienceData[] = [
     beginDate: moment("2018-05").format("MM/YYYY"),
     endDate: moment("2021-05").format("MM/YYYY"),
     jobTitle: "Associate Frontend Developer",
-    duties: [
-      `Example of customers - William Hill, Golden
-          Nugget & Danske Spil`,
-      `Technologies: React, SCSS`,
-    ],
+    duties: [`React, SASS.`],
   },
   {
     companyName: "Silicon Rhino",
@@ -39,9 +31,6 @@ export const experienceData: JobExperienceData[] = [
     beginDate: moment("2017-07").format("MM/YYYY"),
     endDate: moment("2018-06").format("MM/YYYY"),
     jobTitle: "Graduate Full Stack Developer",
-    duties: [
-      `Examples of applications worked on - Bubble, FalconDHQ and Polimapper.`,
-      `Technologies: React, Angular, Typescript, Sass, Node, MongoDB.`,
-    ],
+    duties: [`React, Angular, Typescript, Sass, Node, MongoDB.`],
   },
 ];
